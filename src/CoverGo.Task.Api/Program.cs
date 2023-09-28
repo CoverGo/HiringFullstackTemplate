eae697852f8e1b3092d4e838d91e45a1e9549da9
Pavel Levchuk <SpaierInstinct@gmail.com>
2023-09-28 13:21:25 +0300

    feat: add vue ui

--- src/CoverGo.Task.Api/Program.cs
+++ src/CoverGo.Task.Api/Program.cs
@@ -16,6 +16,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors(it => it.AllowAnyOrigin());
+}
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
